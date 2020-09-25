1720dab0fe49a1b6ccac3af24ce5662b85b8a391
xieyue <xieyue@huya.com>
2020-09-25 15:42:50 +0800

    init

---
+++ config/defaultSettings.js
@@ -0,0 +1,16 @@
+const proSettings = {
+  "navTheme": "light",
+  "primaryColor": "#FAAD14",
+  "layout": "side",
+  "contentWidth": "Fluid",
+  "fixedHeader": false,
+  "fixSiderbar": true,
+  "menu": {
+    "locale": true
+  },
+  "title": "Ant Design Pro",
+  "pwa": false,
+  "iconfontUrl": ""
+}
+
+export default proSettings;
